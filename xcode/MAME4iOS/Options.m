d79bc55c371090ff5308eab51b8b5fc1fa3a8c19
MrJs <30782821+mrjschulte@users.noreply.github.com>
2020-06-12 21:11:19 -0700

    Add in megaTron presets
    This adds in a few presets for the megaTron shader, Shadow Mask,Shadow Mask Strong, Grille Mask, Grille Mask Lite, No Shadow Mask but with Blur.

--- xcode/MAME4iOS/Options.m
+++ xcode/MAME4iOS/Options.m
@@ -87,27 +87,35 @@
 //  you dont use commas to separate values, use spaces.
 //  min, max, and step are all optional, and only effect the InfoHUD, MetalView ignores them.
 //
+//  Presets are simply the @string without the key names, only numbers!
+//
 // NOTE arrayCoreGraphicsEffects and arrayMetalEffects should use the same friendly name
 // for similar effects, so if the user turns off/on metal the choosen effect wont get reset to default.
 //
+#pragma mark - Shader Presets
 + (NSArray*)arrayMetalEffects {
     return @[@"None",
              @"Simple CRT: simpleCRT, mame-screen-dst-rect, mame-screen-src-rect,\
-                 Vertical Curvature     = 5.0 1.0 10.0 0.1,\
-                 Horizontal Curvature   = 4.0 1.0 10.0 0.1,\
-                 Curvature Strength = 0.25 0.0 1.0 0.05,\
-                 Light Boost = 1.3 0.1 3.0 0.1, \
-                 Vignette Strength = 0.05 0.0 1.0 0.05,\
-                 Zoom Factor      = 1.0 0.01 5.0 0.1",
-             @"megaTron: megaTron, mame-screen-src-rect, mame-screen-dst-rect,\
+                            Vertical Curvature = 5.0 1.0 10.0 0.1,\
+                            Horizontal Curvature = 4.0 1.0 10.0 0.1,\
+                            Curvature Strength = 0.25 0.0 1.0 0.05,\
+                            Light Boost = 1.3 0.1 3.0 0.1, \
+                            Vignette Strength = 0.05 0.0 1.0 0.05,\
+                            Zoom Factor = 1.0 0.01 5.0 0.1",
+             @"megaTron - Shadow Mask: megaTron, mame-screen-src-rect, mame-screen-dst-rect,\
                             Shadow Mask Type = 3.0 0.0 3.0 1.0,\
                             Shadow Mask Intensity = 0.5 0.0 1.0 0.05,\
-                            Scanline Thinness = 0.7 0.0 1.0 0.1,\
-                            Horizontal Scanline Blur = 2.5 1.0 3.0 0.1,\
+                            Scanline Thinness = 0.7 0.0 1.0 0.05,\
+                            Horizontal Scanline Blur = 1.8 1.0 3.0 0.05,\
                             CRT Curvature = 0.02 0.0 0.25 0.01,\
                             Use Trinitron-style Curvature = 0.0 0.0 1.0 1.0,\
                             CRT Corner Roundness = 3.0 2.0 11.0 1.0,\
-                            CRT Gamma = 2.4 0.0 5.0 0.1",
+                            CRT Gamma = 2.9 0.0 5.0 0.1",
+             @"megaTron - Shadow Mask Strong: megaTron, mame-screen-src-rect, mame-screen-dst-rect,3.0,0.75,0.6,1.4,0.02,1.0,2.0,3.0",
+             @"megaTron - Grille Mask: megaTron, mame-screen-src-rect, mame-screen-dst-rect,2.0,0.85,0.6,2.0,0.02,1.0,2.0,3.0",
+             @"megaTron - Grille Mask Lite: megaTron, mame-screen-src-rect, mame-screen-dst-rect,1.0,0.6,0.6,1.6,0.02,1.0,2.0,2.8",
+             @"megaTron - No Shadow Mask but with Blur: megaTron, mame-screen-src-rect, mame-screen-dst-rect,0.0,0.6,0.6,1.0,0.02,0.0,2.0,2.6",
+             
 #ifdef DEBUG
              @"Wombat1: mame_screen_test, mame-screen-size, frame-count, 1.0, 8.0, 8.0",
              @"Wombat2: mame_screen_test, mame-screen-size, frame-count, wombat_rate=2.0, wombat_u=16.0, wombat_v=16.0",
