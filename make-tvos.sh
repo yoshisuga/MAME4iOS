81323480231ce25de469a1d2f1086410affbc84f
MrJs <30782821+mrjschulte@users.noreply.github.com>
2020-08-22 13:09:02 -0700

    Update make-tvos.sh
    sync up w Xcode default build version

--- make-tvos.sh
+++ make-tvos.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 make tvOS=1 clean
-make tvOS=1 -j`sysctl -n hw.logicalcpu` CDBG=-w
+make tvOS=1 -j`sysctl -n hw.logicalcpu` CDBG=-w OSVERSION=12.4
 open xcode/MAME4iOS/MAME4iOS.xcodeproj/
