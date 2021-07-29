16fd735dbf80843d84b1e9cce5dc5ed74f1f5245
Todd Laney <ToddLa@HotMail.com>
2021-07-29 11:47:36 -0700

    remove the arch specific libs, after lipo

--- make-mac.sh
+++ make-mac.sh
@@ -6,6 +6,6 @@ make macCatalyst=1 ARCH=x86_64 clean
 make macCatalyst=1 ARCH=x86_64 -j`sysctl -n hw.logicalcpu` CDBG=-w || exit -1
 
 lipo -create libmame-139u1-mac-*.a -output libmame-139u1-mac.a
-rm libmame-mac-*.a
+rm libmame-139u1-mac-*.a
 
 open xcode/MAME4iOS/MAME4iOS.xcodeproj/
